0d70985cb5555745cc1d9021e6bac890d7490253
HowellYang <th15817161961@gmail.com>
2019-03-22 19:59:43 +0800

    orchestration

--- devops/docker-file/redis-cluster/sentinel/start.sh
+++ devops/docker-file/redis-cluster/sentinel/start.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 cd /etc/redis/
 
-if [[ ! -f "/etc/redis/sentinel.conf" ]]; then
-    CONFIG="/etc/redis/sentinel.conf"
 
-    echo "sentinel monitor mymaster $Monitor" >> "$CONFIG"
+CONFIG="/etc/redis/sentinel.conf"
+echo "sentinel monitor mymaster $Monitor" >> "$CONFIG"
+echo "sentinel config-epoch mymaster 0" >> "$CONFIG"
+echo "sentinel leader-epoch mymaster 0" >> "$CONFIG"
 
-    for server in $KnownSlave; do
-        echo "sentinel known-slave mymaster  $server 6379" >> "$CONFIG"
-    done
-    for server in $KnownSentinel; do
-        echo "sentinel known-sentinel mymaster $server 26379" >> "$CONFIG"
-    done
-fi
+for server in $KnownSlave; do
+    echo "sentinel known-slave mymaster  $server 6379" >> "$CONFIG"
+done
+for server in $KnownSentinel; do
+    echo "sentinel known-sentinel mymaster $server 26379" >> "$CONFIG"
+done
+echo "sentinel current-epoch 0" >> "$CONFIG"
+
+
 
 exec docker-entrypoint.sh redis-sentinel /etc/redis/sentinel.conf --sentinel
--- devops/docker-file/redis-cluster/slave/start.sh
+++ devops/docker-file/redis-cluster/slave/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /etc/redis/
-exec nohup redis-server /etc/redis/redis.conf --protected-mode no > redis.log 2>&1&
+exec redis-server /etc/redis/redis.conf --protected-mode no
